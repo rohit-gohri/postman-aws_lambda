be2dc9012c8883ed1dc7ced6df32f14c5b79e722
Rohit Gohri <gohri.rohit@gmail.com>
2019-10-27 17:15:20 +0530

    add basic lambda

---
+++ config.js
@@ -0,0 +1,8 @@
+module.exports = {
+    db: {
+        host: 'db.region.rds.amazonaws.com',
+        port: 3306,
+        user: 'admin',
+        password: 'password',
+    },
+};
---
+++ src/index.js
@@ -0,0 +1,12 @@
+import cfg from '@smpx/cfg';
+import knex from 'knex';
+
+const METRIC = 'auto-increment-capacity'
+
+/**
+ * @param {import('aws-lambda').APIGatewayEvent} event
+ * @param {import('aws-lambda').Context} context
+ * @param {import('aws-lambda').Callback} callback
+ */
+exports.handler = (event, context, callback) => {
+};
