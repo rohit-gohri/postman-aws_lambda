afada1d2924687d557e374e120ac83a322307915
Rohit Gohri <gohri.rohit@gmail.com>
2019-10-27 18:46:36 +0530

    Make hosts array, add basic script to get metrics

--- config.js
+++ config.js
@@ -1,6 +1,7 @@
 module.exports = {
+    hosts: ['db.region.rds.amazonaws.com'],
+
     db: {
-        host: 'db.region.rds.amazonaws.com',
         port: 3306,
         user: 'admin',
         password: 'password',
--- src/index.js
+++ src/index.js
@@ -1,12 +1,168 @@
 import cfg from '@smpx/cfg';
 import knex from 'knex';
 
-const METRIC = 'auto-increment-capacity'
+const METRIC = 'auto-increment-capacity';
+
+const BLACKLIST_DBS = [
+  'information_schema',
+  'innodb',
+  'mysql',
+  'performance_schema',
+  'sys',
+];
+
+async function getDBs(connection) {
+  const [rows] = await connection.raw('SHOW DATABASES');
+  return rows.map((r) => r.Database).filter((db) => !BLACKLIST_DBS.includes(db));
+}
+
+/**
+ * @typedef {{
+ *  TABLE_SCHEMA: string;
+ *  TABLE_NAME: string;
+ *  COLUMN_NAME: string;
+ *  DATA_TYPE: string;
+ *  COLUMN_TYPE: string;
+ * }} ColumnDetails
+ */
+
+/**
+ *
+ * @param {ColumnDetails} column
+ */
+function getMaxValue(column) {
+  if (column.DATA_TYPE === 'int') {
+    if (column.COLUMN_TYPE.includes('unsigned')) {
+      return 1000;
+    }
+    return 1000;
+  }
+  return 1000;
+}
+
+/**
+ * @typedef {ColumnDetails & {
+ *  MAX_VAL: number;
+ *  PERCENTAGE?: number;
+ * }} ColumnDetailsExt
+ */
+
+/**
+ * @param {knex} connection
+ */
+async function getTableMapForAutoIncrementColumns(connection) {
+  /** @type {ColumnDetails[]} */
+  const columns = await connection.queryBuilder()
+    .table('INFORMATION_SCHEMA.COLUMNS')
+    .whereRaw('?? LIKE ?', ['EXTRA', '%auto_increment%'])
+    .select('TABLE_SCHEMA', 'TABLE_NAME', 'COLUMN_NAME', 'DATA_TYPE', 'COLUMN_TYPE');
+
+  /**
+   * @type {{
+   *  [db: string]: {
+   *      [table: string]: ColumnDetailsEx
+   * }}}
+   */
+  const tableMap = {};
+
+  columns.forEach((column) => {
+    tableMap[column.TABLE_SCHEMA] = tableMap[column.TABLE_SCHEMA] || {};
+    tableMap[column.TABLE_SCHEMA][column.TABLE_NAME] = {
+      ...column,
+      MAX_VAL: getMaxValue(column),
+    };
+  });
+
+  return tableMap;
+}
+
+/**
+ * @typedef {{
+ *  AUTO_INCREMENT: number;
+ *  TABLE_NAME: string;
+ *  TABLE_SCHEMA: string;
+ *  TABLE_ROWS: number;
+ * }} TableDetails
+ */
+
+/**
+ * @param {knex} connection
+ */
+async function getTableMapForAutoIncrementValues(connection) {
+  /** @type {TableDetails[]} */
+  const tables = await connection.queryBuilder()
+    .table('INFORMATION_SCHEMA.TABLES')
+    .whereNotNull('AUTO_INCREMENT')
+    .select('AUTO_INCREMENT', 'TABLE_NAME', 'TABLE_SCHEMA', 'TABLE_ROWS');
+
+  /**
+   * @type {{
+   *  [db: string]: {
+   *      [table: string]: TableDetails
+   * }}}
+   */
+  const tableMap = {};
+
+  tables.forEach((table) => {
+    tableMap[table.TABLE_SCHEMA] = tableMap[table.TABLE_SCHEMA] || {};
+    tableMap[table.TABLE_SCHEMA][table.TABLE_NAME] = table;
+  });
+
+  return tableMap;
+}
 
 /**
  * @param {import('aws-lambda').APIGatewayEvent} event
  * @param {import('aws-lambda').Context} context
  * @param {import('aws-lambda').Callback} callback
  */
-exports.handler = (event, context, callback) => {
+exports.handler = async (event, context, callback) => {
+  const hosts = cfg('hosts', []);
+  const connections = hosts.map((host) => {
+    const connection = { ...cfg('db'), host };
+    return knex({
+      connection,
+      client: 'mysql',
+    });
+  });
+
+  console.log(`Connected to ${connections.length} host(s)!`);
+
+  const columnMaps = await Promise.all(connections
+    .map(getTableMapForAutoIncrementColumns));
+
+  const tableMaps = await Promise.all(connections
+    .map(getTableMapForAutoIncrementValues));
+
+  const metrics = [];
+
+  tableMaps.forEach((tableMap, index) => {
+    const columnMap = columnMaps[index];
+
+    Object.keys(tableMap).forEach((db) => {
+      const tableDetailMap = tableMap[db];
+      Object.keys(tableDetailMap).forEach((table) => {
+        /** @type {ColumnDetailsExt} */
+        const columnDetails = columnMap[db] && columnMap[db][table];
+        /** @type {TableDetails} */
+        const tableDetails = tableDetailMap[table];
+
+        columnDetails.PERCENTAGE = (tableDetails.AUTO_INCREMENT / columnDetails.MAX_VAL) * 100;
+
+        metrics.push(columnDetails);
+      });
+    });
+  });
+
+  metrics.sort((a, b) => b.PERCENTAGE - a.PERCENTAGE);
+
+  console.log(JSON.stringify(metrics, null, 4));
 };
+
+exports.handler().then(() => {
+  console.log('success');
+  process.exit(0);
+}).catch((err) => {
+  console.error(err);
+  process.exit(1);
+});
